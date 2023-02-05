fd112de226d91af4b3e8a93e83438aa12d8f7e12
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-05 01:25:32 +0000

    add contexts

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from "react";
+
+interface contextInterface {
+	theme: boolean;
+	setTheme: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const ThemeContext = createContext<contextInterface>({
+	theme: false,
+	setTheme: () => {},
+});
+
+type Prop = {
+	children: React.ReactNode;
+};
+
+function ContextProvider({ children }: Prop) {
+	const [theme, setTheme] = useState<boolean>(false);
+	return (
+		<ThemeContext.Provider value={{ theme, setTheme }}>
+			{children}
+		</ThemeContext.Provider>
+	);
+}
+
+export default ContextProvider;
