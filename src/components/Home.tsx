78b0b9ac89a77e50628cf1afc03675aa24883fe4
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-05 00:39:36 +0000

    add home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,11 +5,10 @@ import git from "../assets/github.svg";
 import twitter from "../assets/twitter.svg";
 import linkedin from "../assets/linkedin.svg";
 import fem from "../assets/frontend.svg";
-
 function Home() {
 	return (
 		<>
-			<div className="min-h-screen w-full background-dark ">
+			<div className="min-h-screen w-full background-dark">
 				<Header />
 				<div className="flex h-full items-start justify-center">
 					{/* ===container=== */}
@@ -35,39 +34,36 @@ function Home() {
                         "
 						>
 							<h2 className="font-bold sm:text-2xl text-xl mb-3">HELLO,</h2>
-							<p className="font-light text-sm sm:text-lg">
+							<p className="font-light text-md sm:text-lg">
 								I’m Kamasah Dickson a frontend developer with a passion for
-								delivering intuitive and engaging user experiences, I bring a
+								delivering intuitive and engaging user experiences. I bring a
 								wealth of technical knowledge, and creativity to every project.
 							</p>
 						</div>
 						{/* ===social-links=== */}
 						<div className="flex items-end justify-center  mx-auto gap-5 py-5 absolute bottom-0 md:flex-col md:right-5 md:h-full">
 							<img
-								className="cursor-poin
-                                ter hover:scale-110 transition-all active:scale-95
-                                w-8 h-8"
+								className="cursor-pointer hover:scale-110 transition-all active:scale-95
+                                w-8 h-8 md:w-10 md:h-10"
 								src={git}
 								alt="github"
 							/>
 							<img
-								className="cursor-poin
-                                ter hover:scale-110 transition-all active:scale-95
-                                w-8 h-8"
+								className="cursor-pointer hover:scale-110 transition-all active:scale-95
+                                w-8 h-8 md:w-10 md:h-10"
 								src={twitter}
 								alt="twitter"
 							/>
 							<img
-								className="cursor-poin
-                                ter hover:scale-110 transition-all active:scale-95
-                                w-8 h-8"
+								className="cursor-pointer hover:scale-110 transition-all active:scale-95
+                                w-8 h-8 md:w-10 md:h-10"
 								src={linkedin}
 								alt="linkedin"
 							/>
 							<img
-								className="cursor-poin
-                                ter hover:scale-110 transition-all active:scale-95
-                                w-8 h-8"
+								className="
+                                cursor-pointer hover:scale-110 transition-all active:scale-95
+                                w-8 h-8 md:w-10 md:h-10"
 								src={fem}
 								alt="frontendMentor.io"
 							/>
