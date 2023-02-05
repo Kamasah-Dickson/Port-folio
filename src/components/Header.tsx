c9f983323f897d9ac591a95dba2ba4ca059fa886
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-05 21:42:57 +0000

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { ThemeContext } from "../context/ThemeContext";
+import MobileMenu from "./MobileMenu";
+import { BiMenuAltLeft } from "react-icons/bi";
 
 function Header() {
+	const { theme, setTheme } = useContext(ThemeContext);
+	const [show, setShow] = useState<boolean>(false);
+
+	useEffect(() => {
+		localStorage.setItem("theme", JSON.stringify(theme));
+	}, [theme]);
+
 	return (
-		<header className="fixed top-5 left-0 w-full z-50 py-1">
-			<div className="flex justify-between items-center z-30 max-width">
-				<h1 className="cursor-pointer text-white text-2xl font-bold">
+		<header className="fixed top-0 shadow-sm left-0 w-full z-50  filter backdrop-saturate-50">
+			<div className="flex justify-between items-center z-30 max-width h-[75px]">
+				<h1 className="cursor-pointer text-white lg:text-2xl md:text-xl text-[20px] font-bold">
 					<a href="/">
 						KAMAS<span className="text-orange active:scale-105">.DEV</span>
 					</a>
 				</h1>
 				<nav>
-					<ul className="flex items-center gap-5">
+					<ul className="hidden md:flex items-center gap-5">
 						<li className="text-white active:scale-x-105 list-none">
 							<a className=" text-sm font-medium before" href="#home">
 								HOME
@@ -42,8 +52,27 @@ function Header() {
 								CONTACT
 							</a>
 						</li>
+						<li
+							className="text-white cursor-pointer"
+							aria-label={theme ? "toggle lightmode" : "toggle darkMode"}
+							tabIndex={0}
+							onClick={() => setTheme((prev) => !prev)}
+						>
+							{theme ? (
+								<MdLightMode color="white" size={20} />
+							) : (
+								<MdDarkMode size={20} />
+							)}
+						</li>
 					</ul>
+					<li
+						className="cursor-pointer md:hidden list-none"
+						onClick={() => setShow(true)}
+					>
+						<BiMenuAltLeft color="white" size={25} />
+					</li>
 				</nav>
+				<MobileMenu show={show} setShow={setShow} />
 			</div>
 		</header>
 	);
