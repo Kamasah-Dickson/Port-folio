774243331bdf73802d22b71f15995f506cb42035
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-06 10:11:42 +0000

    update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,20 +12,20 @@ function About() {
 			} overflow-hidden h-[500px] `}
 		>
 			{/* ====overlay==== */}
-			<div className="absolute md:right-0 md:left-[unset] left-0 right-[unset] top-0 -z-10 w-[65%] h-[100%] ">
+			<div className="absolute right-0  top-0 -z-10 w-[50%] h-[100%] ">
 				{/* ===img=== */}
 				<div className={`${theme ? "overlay-dark" : "overlay-light"}`}></div>
 			</div>
 			<div className="max-width h-full">
 				<div
-					className={`relative top-36 md:left-24 shadow-md lg:left-32 p-7 rounded-lg md:max-w-lg max-w-xl -z-10 ${
+					className={`relative top-36 md:left-24 lg:left-32 p-7 rounded-lg md:max-w-lg max-w-xl -z-10 ${
 						theme ? "opa-bg" : "opa-bg-2"
 					}`}
 				>
-					<h2 className="text-white text-left text-3xl md:text-2xl lg:text-4xl uppercase font-semibold z-10 mb-2 ">
+					<h2 className="z-10 relative text-white text-left text-3xl md:text-2xl lg:text-4xl uppercase font-semibold mb-3 ">
 						About
 					</h2>
-					<p className="text-gray-200 font-light">
+					<p className="z-10 text-gray-200 font-light">
 						I strive to stay at the forefront of industry trends and
 						advancements, constantly seeking new and innovative ways to deliver
 						value to my clients. Whether you are looking for a partner to help
