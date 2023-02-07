0c773d521b71cec707ee0244d9014764a16ae8ec
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-07 14:10:47 +0000

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,44 +14,74 @@ function Header() {
 
 	return (
 		<header
-			className={`fixed top-0 left-0 z-50 w-full shadow-lg backdrop-saturate-150 ${
-				theme ? "mix-blend-normal filter" : " filter2"
+			className={`fixed top-0 left-0 z-50 w-full shadow-md ${
+				theme
+					? "filter backdrop-saturate-150"
+					: " filter2 backdrop-saturate-[3]"
 			}`}
 		>
 			<div className="max-width z-30 flex h-[75px] items-center justify-between">
-				<h1 className="cursor-pointer text-[20px] font-bold text-white md:text-xl lg:text-2xl">
+				<h1
+					className={`cursor-pointer text-[20px] font-bold ${
+						theme ? "text-white" : "text-gray-700"
+					} md:text-xl lg:text-2xl`}
+				>
 					<a href="/">
 						KAMAS<span className="text-orange active:scale-105">.DEV</span>
 					</a>
 				</h1>
 				<nav>
-					<ul className="hidden items-center gap-5 md:flex">
-						<li className="list-none text-white active:scale-x-105">
+					<ul className="hidden items-center gap-5  md:flex">
+						<li
+							className={`list-none ${
+								theme ? "text-white" : "text-gray-700"
+							} active:scale-x-105`}
+						>
 							<a className=" before text-sm font-medium" href="#home">
 								HOME
 							</a>
 						</li>
-						<li className="text-white active:scale-x-105">
+						<li
+							className={`list-none ${
+								theme ? "text-white" : "text-gray-700"
+							} active:scale-x-105`}
+						>
 							<a className=" before text-sm font-medium" href="#about">
 								ABOUT
 							</a>
 						</li>
-						<li className="text-white active:scale-x-105">
+						<li
+							className={`list-none ${
+								theme ? "text-white" : "text-gray-700"
+							} active:scale-x-105`}
+						>
 							<a className=" before text-sm font-medium" href="#hire-me">
 								HIRE ME
 							</a>
 						</li>
-						<li className="text-white active:scale-x-105">
+						<li
+							className={`list-none ${
+								theme ? "text-white" : "text-gray-700"
+							} active:scale-x-105`}
+						>
 							<a className=" before text-sm font-medium" href="#technologies">
 								TECHNOLOGIES
 							</a>
 						</li>
-						<li className="text-white active:scale-x-105">
+						<li
+							className={`list-none ${
+								theme ? "text-white" : "text-gray-700"
+							} active:scale-x-105`}
+						>
 							<a className=" before text-sm font-medium" href="#projects">
 								PROJECTS
 							</a>
 						</li>
-						<li className="text-white active:scale-x-105">
+						<li
+							className={`list-none ${
+								theme ? "text-white" : "text-gray-700"
+							} active:scale-x-105`}
+						>
 							<a className=" before text-sm font-medium" href="#contact">
 								CONTACT
 							</a>
@@ -65,7 +95,7 @@ function Header() {
 							{theme ? (
 								<MdLightMode color="white" size={20} />
 							) : (
-								<MdDarkMode size={20} />
+								<MdDarkMode size={20} className="text-gray-700" />
 							)}
 						</li>
 					</ul>
@@ -79,14 +109,18 @@ function Header() {
 							{theme ? (
 								<MdLightMode color="white" size={20} />
 							) : (
-								<MdDarkMode size={20} />
+								<MdDarkMode size={20} className="text-gray-700" />
 							)}
 						</li>
 						<li
 							className="cursor-pointer list-none"
 							onClick={() => setShow(true)}
 						>
-							<BiMenuAltLeft color="white" size={25} />
+							{theme ? (
+								<BiMenuAltLeft color="white" size={25} />
+							) : (
+								<BiMenuAltLeft size={25} className="text-gray-700" />
+							)}
 						</li>
 					</ul>
 				</nav>
