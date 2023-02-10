29ebd64fe6323cee514ffbafa2bd8e65d6b68649
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-10 00:17:09 +0000

    update contexts

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,13 +1,16 @@
 import React, { createContext, useState } from "react";
-
 interface contextInterface {
 	theme: boolean;
+	show: boolean;
 	setTheme: React.Dispatch<React.SetStateAction<boolean>>;
+	setShow: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const ThemeContext = createContext<contextInterface>({
 	theme: true,
+	show: false,
 	setTheme: () => {},
+	setShow: () => {},
 });
 
 type Prop = {
@@ -18,9 +21,10 @@ function ContextProvider({ children }: Prop) {
 	const [theme, setTheme] = useState<boolean>(
 		JSON.parse(localStorage.getItem("theme") || "true")
 	);
+	const [show, setShow] = useState<boolean>(false);
 
 	return (
-		<ThemeContext.Provider value={{ theme, setTheme }}>
+		<ThemeContext.Provider value={{ theme, show, setShow, setTheme }}>
 			{children}
 		</ThemeContext.Provider>
 	);
