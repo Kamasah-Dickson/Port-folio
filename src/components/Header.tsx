36a91efd5072f211cc381f90cfde2eb3781881e2
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-10 00:10:59 +0000

    update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,21 @@ import { MdLightMode, MdDarkMode } from "react-icons/md";
 import { ThemeContext } from "../context/ThemeContext";
 import MobileMenu from "./MobileMenu";
 import { BiMenuAltLeft } from "react-icons/bi";
+import { motion, AnimatePresence } from "framer-motion";
+import { container, item } from "../animation";
 
 function Header() {
-	const { theme, setTheme } = useContext(ThemeContext);
-	const [show, setShow] = useState<boolean>(false);
+	const { theme, setTheme, show, setShow } = useContext(ThemeContext);
 
 	useEffect(() => {
 		localStorage.setItem("theme", JSON.stringify(theme));
 	}, [theme]);
 
 	return (
-		<header
+		<motion.header
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 1 }}
 			className={`fixed top-0 left-0 z-50 w-full shadow-md ${
 				theme
 					? "filter backdrop-saturate-150"
@@ -31,76 +35,98 @@ function Header() {
 					</a>
 				</h1>
 				<nav>
-					<ul className="hidden items-center gap-5  md:flex">
-						<li
-							className={`list-none ${
-								theme ? "text-white" : "text-gray-700"
-							} active:scale-x-105`}
+					<AnimatePresence>
+						<motion.ul
+							key={3}
+							variants={container}
+							initial="hidden"
+							animate="show"
+							className="hidden items-center gap-5  md:flex"
 						>
-							<a className=" before text-sm font-medium" href="#home">
-								HOME
-							</a>
-						</li>
-						<li
-							className={`list-none ${
-								theme ? "text-white" : "text-gray-700"
-							} active:scale-x-105`}
-						>
-							<a className=" before text-sm font-medium" href="#about">
-								ABOUT
-							</a>
-						</li>
-						<li
-							className={`list-none ${
-								theme ? "text-white" : "text-gray-700"
-							} active:scale-x-105`}
-						>
-							<a className=" before text-sm font-medium" href="#hire-me">
-								HIRE ME
-							</a>
-						</li>
-						<li
-							className={`list-none ${
-								theme ? "text-white" : "text-gray-700"
-							} active:scale-x-105`}
-						>
-							<a className=" before text-sm font-medium" href="#technologies">
-								TECHNOLOGIES
-							</a>
-						</li>
-						<li
-							className={`list-none ${
-								theme ? "text-white" : "text-gray-700"
-							} active:scale-x-105`}
-						>
-							<a className=" before text-sm font-medium" href="#projects">
-								PROJECTS
-							</a>
-						</li>
-						<li
-							className={`list-none ${
-								theme ? "text-white" : "text-gray-700"
-							} active:scale-x-105`}
-						>
-							<a className=" before text-sm font-medium" href="#contact">
-								CONTACT
-							</a>
-						</li>
-						<li
-							className="cursor-pointer text-white"
-							aria-label={theme ? "toggle lightmode" : "toggle darkMode"}
-							tabIndex={0}
-							onClick={() => setTheme((prev) => !prev)}
-						>
-							{theme ? (
-								<MdLightMode color="white" size={20} />
-							) : (
-								<MdDarkMode size={20} className="text-gray-700" />
-							)}
-						</li>
-					</ul>
-					<ul className="flex items-center gap-3 md:hidden">
-						<li
+							<motion.li
+								variants={item}
+								className={`list-none ${
+									theme ? "text-white" : "text-gray-700"
+								} active:scale-x-105`}
+							>
+								<a className=" before text-sm font-medium" href="#home">
+									HOME
+								</a>
+							</motion.li>
+							<motion.li
+								variants={item}
+								className={`list-none ${
+									theme ? "text-white" : "text-gray-700"
+								} active:scale-x-105`}
+							>
+								<a className=" before text-sm font-medium" href="#about">
+									ABOUT
+								</a>
+							</motion.li>
+							<motion.li
+								variants={item}
+								className={`list-none ${
+									theme ? "text-white" : "text-gray-700"
+								} active:scale-x-105`}
+							>
+								<a className=" before text-sm font-medium" href="#hire-me">
+									HIRE ME
+								</a>
+							</motion.li>
+							<motion.li
+								variants={item}
+								className={`list-none ${
+									theme ? "text-white" : "text-gray-700"
+								} active:scale-x-105`}
+							>
+								<a className=" before text-sm font-medium" href="#technologies">
+									TECHNOLOGY
+								</a>
+							</motion.li>
+							<motion.li
+								variants={item}
+								className={`list-none ${
+									theme ? "text-white" : "text-gray-700"
+								} active:scale-x-105`}
+							>
+								<a className=" before text-sm font-medium" href="#projects">
+									PROJECTS
+								</a>
+							</motion.li>
+							<motion.li
+								variants={item}
+								className={`list-none ${
+									theme ? "text-white" : "text-gray-700"
+								} active:scale-x-105`}
+							>
+								<a className=" before text-sm font-medium" href="#contact">
+									CONTACT
+								</a>
+							</motion.li>
+							<motion.li
+								variants={item}
+								className="cursor-pointer text-white"
+								aria-label={theme ? "toggle lightmode" : "toggle darkMode"}
+								tabIndex={0}
+								onClick={() => setTheme((prev) => !prev)}
+							>
+								{theme ? (
+									<MdLightMode color="white" size={20} />
+								) : (
+									<MdDarkMode size={20} className="text-gray-700" />
+								)}
+							</motion.li>
+						</motion.ul>
+					</AnimatePresence>
+					<motion.ul
+						key={4}
+						variants={container}
+						initial="hidden"
+						animate="show"
+						className="flex items-center gap-3 md:hidden"
+					>
+						<motion.li
+							variants={item}
 							className="cursor-pointer list-none text-white"
 							aria-label={theme ? "toggle lightmode" : "toggle darkMode"}
 							tabIndex={0}
@@ -111,8 +137,9 @@ function Header() {
 							) : (
 								<MdDarkMode size={20} className="text-gray-700" />
 							)}
-						</li>
-						<li
+						</motion.li>
+						<motion.li
+							variants={item}
 							className="cursor-pointer list-none"
 							onClick={() => setShow(true)}
 						>
@@ -121,13 +148,13 @@ function Header() {
 							) : (
 								<BiMenuAltLeft size={25} className="text-gray-700" />
 							)}
-						</li>
-					</ul>
+						</motion.li>
+					</motion.ul>
 				</nav>
 
 				<MobileMenu show={show} setShow={setShow} />
 			</div>
-		</header>
+		</motion.header>
 	);
 }
 
