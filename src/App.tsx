439091e7edee9bc6fc21ebce25fbcbbaa8be326e
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-10 00:14:51 +0000

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Home from "./components/Home";
 import About from "./components/About";
 import "./index.css";
-// import { motion } from "framer-motion";
 import ContextProvider from "./context/ThemeContext";
 import HireMe from "./components/HireMe";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 export default function App() {
 	return (
 		<>
@@ -15,30 +16,9 @@ export default function App() {
 				<HireMe />
 				<Technologies />
 				<Projects />
+				<Contact />
+				<Footer />
 			</ContextProvider>
 		</>
 	);
 }
-
-// import React from "react";
-// import { motion, useViewportScroll } from "framer-motion";
-
-// const ScrollReveal = ({ children }) => {
-//   const { scrollY } = useViewportScroll();
-
-//   return (
-//     <motion.div
-//       style={{
-//         opacity: scrollY.interpolate({
-//           inputRange: [100, 200],
-//           outputRange: [0, 1],
-//         }),
-//         transition: { duration: 0.5 },
-//       }}
-//     >
-//       {children}
-//     </motion.div>
-//   );
-// };
-
-// export default ScrollReveal;
