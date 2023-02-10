15d148f28e3b60e78c096f5741a194c4012a847a
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-10 22:30:21 +0000

    update Technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,10 +1,9 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-
 import react from "../assets/react.svg";
 import javaScript from "../assets/JS.svg";
 import typeScript from "../assets/Typescript.svg";
-import gsap from "../assets/greensock.svg";
+import framer from "../assets/framer-motion-seeklogo.com.svg";
 import sass from "../assets/sass.svg";
 import redux from "../assets/REDUX.svg";
 import git from "../assets/git.svg";
@@ -19,7 +18,7 @@ function Technologies() {
 	const { theme } = useContext(ThemeContext);
 	return (
 		<div
-			className="  tech relative h-full overflow-hidden pt-48 pb-36 "
+			className="  tech relative h-full overflow-hidden pt-40 pb-40"
 			id="technologies"
 		>
 			<div className="overflow-hidden">
@@ -30,6 +29,7 @@ function Technologies() {
 						y: "0",
 						transition: { delay: 1, duration: 1.2 },
 					}}
+					viewport={{ once: true }}
 					className={`relative z-10 ${
 						!theme ? "text-dark-bg" : "text-white"
 					}  mb-5 text-center text-3xl font-semibold uppercase md:text-2xl lg:text-4xl`}
@@ -43,6 +43,7 @@ function Technologies() {
 						y: "0",
 						transition: { delay: 1.2, duration: 1.2 },
 					}}
+					viewport={{ once: true }}
 					className={`relative z-10 ${
 						!theme ? "text-dark-bg" : "text-white"
 					}  text-center  text-lg font-medium uppercase`}
@@ -55,81 +56,94 @@ function Technologies() {
 				variants={container}
 				initial="hidden"
 				whileInView="show"
+				viewport={{ once: true }}
 				className="max-width my-grid2 relative z-10 mt-16 flex h-full flex-wrap gap-5"
 			>
 				<motion.a
 					variants={item}
-					href="#"
+					title="React"
+					href="https://reactjs.org/"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={react} alt="React js" />
 				</motion.a>
 				<motion.a
+					title="JavaScript"
 					variants={item}
-					href="#"
+					href="https://www.javascript.com/"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={javaScript} alt="javaScript" />
 				</motion.a>
 				<motion.a
 					variants={item}
-					href="#"
+					title="TypeScript"
+					href="https://www.typescriptlang.org/"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={typeScript} alt="typeScript" />
 				</motion.a>
+
 				<motion.a
 					variants={item}
-					href="#"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={gsap} alt="gsap" />
-				</motion.a>
-				<motion.a
-					variants={item}
-					href="#"
+					title="Sass"
+					href="https://sass-lang.com/"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={sass} alt="gsap" />
 				</motion.a>
 				<motion.a
 					variants={item}
-					href="#"
+					title="Redux"
+					href="https://redux.js.org/"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={redux} alt="redux" />
 				</motion.a>
 				<motion.a
+					title="Git"
 					variants={item}
-					href="#"
+					href="https://git-scm.com/"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={git} alt="git" />
 				</motion.a>
 				<motion.a
+					title="Html"
 					variants={item}
-					href="#"
+					href="https://www.w3schools.com/html/"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={html} alt="html" />
 				</motion.a>
 				<motion.a
+					title="Css"
 					variants={item}
-					href="#"
+					href="https://www.w3schools.com/Css/"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={css} alt="css" />
 				</motion.a>
 				<motion.a
+					title="Github"
 					variants={item}
-					href="#"
+					href="https://github.com/Kamasah-Dickson"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={github} alt="github" />
 				</motion.a>
 				<motion.a
 					variants={item}
-					href="#"
+					title="framer motion"
+					href="https://www.framer.com/"
+					className="transition-all hover:scale-110 active:scale-95"
+				>
+					<img src={framer} alt="framer motion" />
+				</motion.a>
+				<motion.a
+					variants={item}
+					href="https://tailwindcss.com/"
+					title="tailwindcss"
 					className="transition-all hover:scale-110 active:scale-95"
 				>
 					<img src={tailwind} alt="tailwind css" />
