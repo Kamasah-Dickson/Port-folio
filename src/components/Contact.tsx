8957f5c1b44897bba20e6619ee663fff7ca921b1
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-10 04:15:30 +0000

    update contact animations

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,6 +6,7 @@ import phoneDark from "../assets/CALL-dark.svg";
 import contactSvg from "../assets/contact.svg";
 import contactSvgLight from "../assets/contact-light-mode.svg";
 import { ThemeContext } from "../context/ThemeContext";
+import { motion } from "framer-motion";
 function Contact() {
 	const { theme } = useContext(ThemeContext);
 
@@ -16,30 +17,80 @@ function Contact() {
 		>
 			<div className="contact-grid max-width contact relative overflow-hidden">
 				<div className={`${theme ? "text-white" : "text-dark-bg"}`}>
-					<h3 className="mb-3 px-3 text-left text-4xl font-bold">Contact</h3>
-					<p className="text-md mb-6 max-w-md px-3  font-light">
-						For business and partnership inquiry please contact me below!
-					</p>
+					<div className="overflow-hidden pb-6">
+						<motion.h3
+							initial={{ opacity: 0, y: "100%" }}
+							whileInView={{
+								opacity: 1,
+								y: "0",
+								transition: { delay: 1, duration: 1 },
+							}}
+							className="mb-3 px-3 text-left text-4xl font-bold"
+						>
+							Contact
+						</motion.h3>
+						<motion.p
+							initial={{ opacity: 0, y: "100%" }}
+							whileInView={{
+								opacity: 1,
+								y: "0",
+								transition: { delay: 1.2, duration: 1 },
+							}}
+							className="text-md  max-w-md px-3  font-light"
+						>
+							For business and partnership inquiry please contact me below!
+						</motion.p>
+					</div>
 					<div className="mt-10 flex flex-col gap-5">
-						<div className="flex items-center gap-5 p-3">
-							<img src={theme ? phone : phoneDark} alt="call" />
-							<div className="flex flex-col gap-[3px]">
-								<h4 className="text-sm font-semibold">PHONE</h4>
-								<p className="text-md font-light">+2335945710</p>
-							</div>
+						<div>
+							<motion.div
+								initial={{ opacity: 0, y: "100%" }}
+								whileInView={{
+									opacity: 1,
+									y: "0",
+									transition: { delay: 1.5, duration: 1 },
+								}}
+								className="flex items-center gap-5 p-3"
+							>
+								<img src={theme ? phone : phoneDark} alt="call" />
+								<div className="flex flex-col gap-[3px]">
+									<h4 className="text-sm font-semibold">PHONE</h4>
+									<p className="text-md font-light">+2335945710</p>
+								</div>
+							</motion.div>
 						</div>
-						<div className="flex w-fit cursor-pointer items-center gap-5 rounded-sm p-3 shadow-sm transition-shadow hover:bg-[#584eac38]">
+						<motion.div
+							initial={{ opacity: 0, y: "100%" }}
+							whileInView={{
+								opacity: 1,
+								y: "0",
+								transition: { delay: 1.7, duration: 1 },
+							}}
+							className={`flex w-fit cursor-pointer items-center gap-5 rounded-sm border ${
+								theme
+									? "bg-[#584eac38]  hover:bg-[#574eac0e]  active:bg-slate-900"
+									: "bg-gray-100 hover:bg-gray-300 active:bg-gray-200"
+							} p-3 shadow-sm transition-shadow `}
+						>
 							<img src={theme ? email : emailDark} alt="" />
 							<div className="flex flex-col gap-[3px]">
 								<h4 className="text-sm font-semibold">EMAIL</h4>
 								<p className="text-md font-light">kamasahdickson19@gmail.com</p>
 							</div>
-						</div>
+						</motion.div>
 					</div>
 				</div>
-				<div className="ml-auto">
+				<motion.div
+					initial={{ opacity: 0, x: "20%" }}
+					whileInView={{
+						opacity: 1,
+						x: "0%",
+						transition: { delay: 2, duration: 1 },
+					}}
+					className="ml-auto"
+				>
 					<img src={theme ? contactSvg : contactSvgLight} alt="contact" />
-				</div>
+				</motion.div>
 			</div>
 		</div>
 	);
