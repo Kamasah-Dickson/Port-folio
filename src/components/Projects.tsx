07cb548067fed4b6733b2a331ed62df3b97077d7
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-10 22:29:49 +0000

    update Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,6 +8,7 @@ import iphone from "../assets/Iphone.svg";
 import cinema from "../assets/cinema.svg";
 import countries from "../assets/countries.svg";
 import form from "../assets/form.svg";
+import github from "../assets/github2.svg";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -20,7 +21,7 @@ function Projects() {
 		<div
 			className={` pt-28 ${
 				theme ? "bg-dark-bg" : "bg-[#60585804]"
-			} project relative overflow-x-hidden `}
+			} project relative mt-20 overflow-x-hidden `}
 			id="projects"
 		>
 			<div className="overflow-hidden ">
@@ -31,6 +32,7 @@ function Projects() {
 						y: "0",
 						transition: { delay: 1, duration: 1 },
 					}}
+					viewport={{ once: true }}
 					className={`z-10 ${
 						!theme ? "text-dark-bg" : "text-white"
 					}  mb-3 text-center text-3xl font-semibold uppercase md:text-2xl lg:text-4xl`}
@@ -44,6 +46,7 @@ function Projects() {
 						y: "0",
 						transition: { delay: 1.2, duration: 1 },
 					}}
+					viewport={{ once: true }}
 					className={`z-10 ${
 						!theme ? "text-gray-600" : "text-gray-300"
 					}  mb-10 text-center text-xl font-normal uppercase lg:text-2xl`}
@@ -58,6 +61,7 @@ function Projects() {
 					x: "0%",
 					transition: { delay: 1.2, duration: 1 },
 				}}
+				viewport={{ once: true }}
 				className="max-width2 relative z-10"
 			>
 				<Swiper
@@ -66,10 +70,7 @@ function Projects() {
 					modules={[Navigation, Pagination, A11y]}
 					spaceBetween={40}
 					breakpoints={{
-						700: {
-							slidesPerView: 2,
-						},
-						900: {
+						1024: {
 							slidesPerView: 2,
 						},
 					}}
@@ -81,12 +82,21 @@ function Projects() {
 							theme ? "my-shadow" : "shadow-2xl"
 						}         w-[300px] cursor-grab rounded-[50px] `}
 					>
-						<div>
+						<div className="relative">
 							<img
 								src={iphone}
 								alt=""
 								className="h-[400px] w-full rounded-[50px] object-cover md:h-[450px]"
 							/>
+							<div
+								className={`absolute bottom-0 top-0 left-0 flex h-[450px] w-[100%] items-center justify-center rounded-[50px] opacity-0 transition-all hover:opacity-100 ${
+									theme ? "hover:bg-[#c77d4c3f]" : "hover:bg-[#80008041]"
+								} object-cover`}
+							>
+								<a href="https://github.com/Kamasah-Dickson">
+									<img className="cursor-pointer" src={github} alt="github" />
+								</a>
+							</div>
 						</div>
 					</SwiperSlide>
 					<SwiperSlide
@@ -94,12 +104,21 @@ function Projects() {
 							theme ? "my-shadow" : "shadow-2xl"
 						}         w-[300px] cursor-grab rounded-[50px] `}
 					>
-						<div>
+						<div className="relative">
 							<img
 								src={nft}
 								alt=""
 								className="h-[400px] w-full rounded-[50px] object-cover md:h-[450px]"
 							/>
+							<div
+								className={`absolute bottom-0 top-0 left-0 flex h-[450px] w-[100%] items-center justify-center rounded-[50px] opacity-0 transition-all hover:opacity-100 ${
+									theme ? "hover:bg-[#c77d4c3f]" : "hover:bg-[#80008041]"
+								} object-cover`}
+							>
+								<a href="https://github.com/Kamasah-Dickson">
+									<img className="cursor-pointer" src={github} alt="github" />
+								</a>
+							</div>
 						</div>
 					</SwiperSlide>
 
@@ -108,33 +127,66 @@ function Projects() {
 							theme ? "my-shadow" : "shadow-2xl"
 						}         w-[300px] cursor-grab rounded-[50px]`}
 					>
-						<img
-							src={cinema}
-							alt=""
-							className="h-[400px] w-full rounded-[50px] object-cover object-top shadow-2xl md:h-[450px]"
-						/>
+						<div className="relative">
+							<img
+								src={cinema}
+								alt=""
+								className="h-[400px] w-full rounded-[50px] object-cover object-top shadow-2xl md:h-[450px]"
+							/>
+							<div
+								className={`absolute bottom-0 top-0 left-0 flex h-[450px] w-[100%] items-center justify-center rounded-[50px] opacity-0 transition-all hover:opacity-100 ${
+									theme ? "hover:bg-[#c77d4c3f]" : "hover:bg-[#80008041]"
+								} object-cover`}
+							>
+								<a href="https://github.com/Kamasah-Dickson">
+									<img className="cursor-pointer" src={github} alt="github" />
+								</a>
+							</div>
+						</div>
 					</SwiperSlide>
 					<SwiperSlide
 						className={`${
 							theme ? "my-shadow" : "shadow-2xl"
 						}         w-[300px] cursor-grab rounded-[50px]`}
 					>
-						<img
-							src={countries}
-							alt=""
-							className="h-[400px] w-full rounded-[50px] object-cover object-top shadow-2xl md:h-[450px]"
-						/>
+						<div className="relative">
+							<img
+								src={countries}
+								alt=""
+								className="h-[400px] w-full rounded-[50px] object-cover object-top shadow-2xl md:h-[450px]"
+							/>
+							<div
+								className={`absolute bottom-0 top-0 left-0 flex h-[450px] w-[100%] items-center justify-center rounded-[50px] opacity-0 transition-all hover:opacity-100 ${
+									theme ? "hover:bg-[#c77d4c3f]" : "hover:bg-[#80008041]"
+								} object-cover`}
+							>
+								<a href="https://github.com/Kamasah-Dickson">
+									<img className="cursor-pointer" src={github} alt="github" />
+								</a>
+							</div>
+						</div>
 					</SwiperSlide>
 					<SwiperSlide
 						className={`${
 							theme ? "my-shadow" : "shadow-2xl"
 						}         w-[300px] cursor-grab rounded-[50px]`}
 					>
-						<img
-							src={form}
-							alt=""
-							className="h-[400px] w-full rounded-[50px] object-cover object-top shadow-2xl md:h-[450px]"
-						/>
+						<div className="relative">
+							<img
+								src={form}
+								alt=""
+								className="h-[400px] w-full rounded-[50px] object-cover object-top shadow-2xl md:h-[450px]"
+							/>
+							<div
+								className={`absolute bottom-0 top-0 left-0 flex h-[450px] w-[100%] items-center justify-center rounded-[50px] opacity-0 transition-all hover:opacity-100 ${
+									theme ? "hover:bg-[#c77d4c3f]" : "hover:bg-[#80008041]"
+								} object-cover`}
+							>
+								<a href="https://github.com/Kamasah-Dickson">
+									<img className="cursor-pointer" src={github} alt="github" />
+								</a>
+							</div>
+						</div>
 					</SwiperSlide>
 					<div className="mt-[50px]"></div>
 				</Swiper>
