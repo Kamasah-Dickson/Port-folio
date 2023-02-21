0eec465749d4cb01cf9df1b7a5ccc25fc6c23539
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-21 04:17:50 +0000

    update animation

--- src/animation.ts
+++ src/animation.ts
@@ -16,3 +16,20 @@ export const item = {
 	hidden: { opacity: 0 },
 	show: { opacity: 1, transition: { duration: 0.5 } },
 };
+
+export const container2 = {
+	hidden: { opacity: 0 },
+	show: {
+		opacity: 1,
+		transition: {
+			delayChildren: 0.3,
+			staggerChildren: 0.13,
+			duration: 1,
+		},
+	},
+};
+
+export const item2 = {
+	hidden: { opacity: 0 },
+	show: { opacity: 1, transition: { duration: 0.2 } },
+};
