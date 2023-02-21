f0519c2562c243ea0b4efc8f4c8d9a44824c710e
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-02-21 04:17:28 +0000

    update APp

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,29 @@ import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./components/NotFound";
 export default function App() {
 	return (
-		<>
-			<ContextProvider>
-				<Home />
-				<About />
-				<HireMe />
-				<Technologies />
-				<Projects />
-				<Contact />
-				<Footer />
-			</ContextProvider>
-		</>
+		<Router>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<ContextProvider>
+							<Home />
+							<About />
+							<HireMe />
+							<Technologies />
+							<Projects />
+							<Contact />
+							<Footer />
+						</ContextProvider>
+					}
+				></Route>
+				<Route path="*" element={<NotFound />}></Route>
+			</Routes>
+		</Router>
 	);
 }
