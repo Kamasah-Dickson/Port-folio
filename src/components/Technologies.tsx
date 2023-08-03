07ac430f0d895887c167b98b68773727ae6d3ac6
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-08-03 10:19:10 +0000

    refactor technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,20 +1,9 @@
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-import react from "../assets/react.svg";
-import javaScript from "../assets/JS.svg";
-import typeScript from "../assets/Typescript.svg";
-import framer from "../assets/framerMotion.svg";
-import sass from "../assets/sass.svg";
-import redux from "../assets/REDUX.svg";
-import git from "../assets/git.svg";
-import html from "../assets/html.svg";
-import css from "../assets/css.svg";
-import github from "../assets/github2.svg";
-import tailwind from "../assets/tailwind.svg";
-import nextJs from "../assets/next-js.svg";
-import firebase from "../assets/firebase.svg";
+
 import { motion } from "framer-motion";
 import { container, item } from "../animation";
+import { techStacks } from "../technologies";
 
 function Technologies() {
 	const { theme } = useContext(ThemeContext);
@@ -29,12 +18,12 @@ function Technologies() {
 					whileInView={{
 						opacity: 1,
 						y: "0",
-						transition: { delay: 1, duration: 1.2 },
+						transition: { delay: 0.4, duration: 1.1 },
 					}}
-					viewport={{ once: true }}
+					// viewport={{ once: true }}
 					className={`relative z-10 ${
 						!theme ? "text-dark-bg" : "text-white"
-					}  mb-5 text-center text-3xl font-semibold uppercase md:text-2xl lg:text-4xl`}
+					}  mb-1 text-center text-3xl font-semibold uppercase md:text-2xl lg:text-5xl`}
 				>
 					MY TECH STACK
 				</motion.h3>
@@ -43,12 +32,12 @@ function Technologies() {
 					whileInView={{
 						opacity: 1,
 						y: "0",
-						transition: { delay: 1.2, duration: 1.2 },
+						transition: { delay: 0.9, duration: 1.1 },
 					}}
-					viewport={{ once: true }}
+					// viewport={{ once: true }}
 					className={`relative z-10 ${
 						!theme ? "text-dark-bg" : "text-white"
-					}  px-1  text-center text-lg font-medium uppercase`}
+					}  px-1 text-center text-lg font-medium uppercase`}
 				>
 					TECHNOLOGIES I'VE BEEN WORKING WITH
 				</motion.h5>
@@ -58,115 +47,28 @@ function Technologies() {
 				variants={container}
 				initial="hidden"
 				whileInView="show"
-				viewport={{ once: true }}
-				className="max-width my-grid2 relative z-10 mt-16 flex h-full flex-wrap gap-5"
+				// viewport={{ once: true }}
+				className="
+				my-grid2
+				relative
+					z-10
+					mx-auto
+				mt-16 flex h-full max-w-7xl flex-wrap gap-5 px-3"
 			>
-				<motion.a
-					variants={item}
-					title="React"
-					href="https://reactjs.org/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={react} alt="React js" />
-				</motion.a>
-				<motion.a
-					title="JavaScript"
-					variants={item}
-					href="https://www.javascript.com/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={javaScript} alt="javaScript" />
-				</motion.a>
-				<motion.a
-					variants={item}
-					title="TypeScript"
-					href="https://www.typescriptlang.org/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={typeScript} alt="typeScript" />
-				</motion.a>
-
-				<motion.a
-					variants={item}
-					title="Sass"
-					href="https://sass-lang.com/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={sass} alt="gsap" />
-				</motion.a>
-				<motion.a
-					variants={item}
-					title="Next js"
-					href="https://nextjs.org/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={nextJs} alt="Next Js" />
-				</motion.a>
-				<motion.a
-					variants={item}
-					title="Redux"
-					href="https://redux.js.org/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={redux} alt="redux" />
-				</motion.a>
-				<motion.a
-					title="Git"
-					variants={item}
-					href="https://git-scm.com/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={git} alt="git" />
-				</motion.a>
-				<motion.a
-					title="Html"
-					variants={item}
-					href="https://www.w3schools.com/html/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={html} alt="html" />
-				</motion.a>
-
-				<motion.a
-					title="firebase"
-					variants={item}
-					href="https://firebase.google.com/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={firebase} alt="firebase" />
-				</motion.a>
-				<motion.a
-					title="Css"
-					variants={item}
-					href="https://www.w3schools.com/Css/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={css} alt="css" />
-				</motion.a>
-				<motion.a
-					title="Github"
-					variants={item}
-					href="https://github.com/Kamasah-Dickson"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={github} alt="github" />
-				</motion.a>
-				<motion.a
-					variants={item}
-					title="framer motion"
-					href="https://www.framer.com/motion/"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={framer} alt="framer motion" />
-				</motion.a>
-				<motion.a
-					variants={item}
-					href="https://tailwindcss.com/"
-					title="tailwindcss"
-					className="transition-all hover:scale-110 active:scale-95"
-				>
-					<img src={tailwind} alt="tailwind css" />
-				</motion.a>
+				{techStacks.map((tech) => {
+					return (
+						<motion.a
+							variants={item}
+							href={tech.link}
+							target="_blank"
+							title={tech.title}
+							className="transition-all hover:scale-[1.04] active:scale-95"
+							key={tech.link}
+						>
+							<img src={tech.img} alt={tech.title} />
+						</motion.a>
+					);
+				})}
 			</motion.div>
 		</div>
 	);
